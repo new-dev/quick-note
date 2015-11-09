42921351101bf5498cfdd5b6782f3b9c476c7a8e
Timothy Jensen <timothyjamesjensen@gmail.com>
2015-11-09 14:44:14 -0800

    Made quicknote logo clickable. On click, user is returned to new-note view.

--- popup.js
+++ popup.js
@@ -48,6 +48,10 @@ app.controller('appCtrl', function($scope, $q){
       displayNoteList();
     };
 
+    $scope.viewCreateNote = function() {
+      displayCreateNote();
+    };
+
     $scope.saveNote = function() {
       var noteText = document.getElementById("note-area").innerText;
       var currentDate = getCurrentDate();
@@ -129,65 +133,48 @@ function displayNoteList() {
       )
 }
 
+function displayCreateNote() {
+  if (document.getElementById('new-note').className != "display-nothing") {
+
+  } else {
+    document.getElementById('note-list').className += "display-nothing";
+    document.getElementById('new-note').className =
+        document.getElementById('new-note').className.replace(
+            /(?:^|\s)display-nothing(?!\S)/g , ''
+        )
+  }
+}
+
 function getCurrentDate() {
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1; //January is 0!
   var yyyy = today.getFullYear();
 
-  if(dd<10) {
-    dd='0'+dd
+  if (dd < 10) {
+    dd = '0' + dd
   }
 
-  if(mm<10) {
-    mm='0'+mm
+  if (mm < 10) {
+    mm = '0' + mm
   }
 
   today = mm+'/'+dd+'/'+yyyy;
   return today;
 }
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
